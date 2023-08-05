e484a97071447e87b8e22628b04f574c79b27354
kaybrian <kayonbrian@gmail.com>
2023-08-05 14:28:09 +0200

    users add to the community

--- tafari/screens/Community.js
+++ tafari/screens/Community.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { StyleSheet, View, ScrollView, Text } from "react-native";
+import React from "react";
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from "react-native";
 import { Image } from "expo-image";
 import TrendingCard from "../components/TrendingCard";
 import ContainerCardForm from "../components/ContainerCardForm";
@@ -7,7 +7,7 @@ import WellnessHubSection from "../components/WellnessHubSection";
 import { Color, FontFamily, FontSize, Padding, Border } from "../GlobalStyles";
 import SectionGreetings from "../components/SectionGreetings";
 import SortCommunity from "../components/SortCommunity";
-
+import { Ionicons } from "@expo/vector-icons";
 
 const Community = () => {
   return (
@@ -18,7 +18,6 @@ const Community = () => {
         horizontal showsHorizontalScrollIndicator={false}
         className="flex flex-row  mx-4 mt-3 mb-3 py-3"
       >
-
         <SortCommunity name="Trending" />
       </View>
 
@@ -51,11 +50,31 @@ const Community = () => {
           timestamp="3 hrs ago"
           imageTimestamp={require("../assets/ellipse-22.png")}
         />
-
       </ScrollView>
+
+      {/* Action Floating Button */}
+      <TouchableOpacity
+        style={styles.floatingButton}
+      >
+        <Ionicons name="pencil" size={24} color="#FFFFFF" />
+      </TouchableOpacity>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: "absolute",
+    bottom: 70,
+    right: 20,
+    backgroundColor: "#FE8235",
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 4,
+  },
+});
 
 export default Community;
